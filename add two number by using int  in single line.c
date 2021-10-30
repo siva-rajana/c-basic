fdb79b8d6fbf2ec23f1784fe7d8d6ee474157584
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-10-30 12:32:03 +0530

    Add files via upload

---
+++ add two number by using int  in single line.c
@@ -0,0 +1,13 @@
+/* adding two numbers by using integer
+  integer means indicates numarical numbers(1,2,3...)*/
+#include<stdio.h>
+void main()
+
+{
+	
+	int num1=25 , num2=45, sum=num1+num2;
+	
+	printf("su of %d and %d is %d",num1,num2,sum);
+	
+}
+
