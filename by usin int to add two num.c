31c8451106c324af68d450c7ef62a8c5cb828a02
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-10-30 11:52:42 +0530

    Add files via upload

---
+++ by usin int to add two num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//varaibles
+	int num1, num2, num3, num4, num5, sum;
+	
+	//initialization
+	num1=40;
+	num2=20;
+	
+	
+	//caluculation
+	sum= num1+num2;
+	
+	//output
+	printf("sum of %d and %d is %d", num1, num2, sum);
+	
+	
+}
