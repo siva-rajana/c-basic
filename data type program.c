b9b2a906ed492cd8ad34394422f745d91e4f49ae
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-10-30 11:52:57 +0530

    Add files via upload

---
+++ data type program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{ 
+//variable declaration
+int age;
+float cgpa;
+char initial;
+
+//initialization
+age=17;
+cgpa=8.2;
+initial='s';
+
+//output
+printf("age of the student is %d\n", age);
+printf("percentage of student is %f\n", cgpa);
+printf("student surname starts with %c", initial);
+
+
+}
