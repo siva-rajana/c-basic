c5385182e0a2e27fd0d1172deea40295ca9f2bcc
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-11-05 23:15:11 -0700

    Add files via upload

---
+++ all arthematics.c
@@ -0,0 +1,31 @@
+//printing all arithematic operatios
+#include<stdio.h>
+void main()
+
+{
+	int a,b,A,S,P,D,M;
+   a=10;
+   b=2;
+   
+   A=10+2;
+   S=10-2;
+   P=10*2;
+   D=10/2;
+   M=10%2;
+   
+   
+   printf("sum of %d and %d is %d\n",a,b, A);
+   printf("sub of %d and %d is %d\n",a,b,S);
+   printf("product of %d and %d is %d\n",a,b,P);
+   printf("division of %d and %d is %d\n",a,b,D);
+   printf("module of %d and %d is %d",a,b,M);
+   
+   
+	
+}
+   
+   
+   
+   
+   
+   
