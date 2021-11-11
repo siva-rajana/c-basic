1fc5c881ecd84e1770a80f1f9ac6ddbdd4466f6b
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-11-10 22:48:05 -0800

    Add files via upload

---
+++ area n permiter if th rectangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	
+	int L,W,s2,a,p;
+	
+	printf("enter the length:");
+	
+	scanf("%d", &L);
+	
+	printf("enter the width:");
+	
+	scanf("%d", &W);
+	a=L*W;
+	p=2*(L+W);
+	
+	printf("area of the rectangle is %d\n",a);
+	printf("perimeter of the rectangle is %d",p);
+	
+	
+	
+}
