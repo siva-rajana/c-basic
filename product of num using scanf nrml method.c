19f9468cfde3a0c58a1505e4446f456a258f8e89
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-11-10 22:14:30 -0800

    Add files via upload

---
+++ product of num using scanf nrml method.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+
+{
+	int a,b,c,d;
+	printf("enter first number");
+	
+	scanf("%d", &a);
+	
+	printf("enter second number");
+	
+	scanf("%d", &b);
+	
+	printf("enter third number");
+	
+	scanf("%d",&c);
+	
+	printf("enter fourth number");
+	
+	scanf("%d", &d);
+	
+	printf("%d",a+b+c+d);
+	
+	
+}
