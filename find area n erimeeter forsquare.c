567f26d470cef2b9ff739c13d526e8229b697eb8
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-11-10 22:34:27 -0800

    Add files via upload

---
+++ find area n erimeeter forsquare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+
+{
+	int s,a,p;
+
+	
+	
+	printf("enter the number");
+	scanf("%d",&s);
+	a=s*s;
+	p=4*s;
+	
+	printf("area of the square is %d\n",a);
+	printf("perimeter of the square is %d", p);
+	
+	
+}
