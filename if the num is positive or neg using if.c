25199a80b3c21587048e03c69125da0d92e8b3ab
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2021-12-10 22:51:14 -0800

    Add files via upload

---
+++ if the num is positive or neg using if.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+
+{
+	int number;
+	
+	scanf("%d",&number);
+	
+	if(number>=0)
+	
+	printf(" the number is positive");
+	else
+	printf(" the number is negative");
+	
+	
+	
+}
