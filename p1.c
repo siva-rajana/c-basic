59d8bdcdd0b5b4af4c493b494893852f9ddb22fb
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-02-23 12:25:36 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	//study about output-->printf()
+	//printf("welcome to c");
+	//int a=10,b=20;
+	//printf("%d %d",a,b);
+	//printf("a=%d and b=%d",a,b);
+	//printf("%d %d",b,a);
+	//printf("%d",b,a);
+    //printf("%d",a,b);
+    //printf("%5d %5d",a,b);
+	//printf("%-4d %-4d",a,b);
+	//printf("%d # %d",a,b);
+	
+	return 0;
+	
+}
+
+
+
+
+
