eb58f1338243b6bd603cf76e0b3a9149d80bfd5a
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-02-24 12:09:51 +0530

    Add files via upload

---
+++ p2_area of recatangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int l,b,a;
+	printf("Enter values of length and bridth of rectangle\n");
+	scanf("%d%d",&l,&b);
+	
+	a=l*b;
+	printf("the area of rectangle  is %d",a);
+	
+	
+	
+	return 0;
+}
