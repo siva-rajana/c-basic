0b3152353415620c62a4ad77159c15ac7b742474
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-02-24 12:09:30 +0530

    Add files via upload

---
+++ p5_simple intrest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r,si;
+	printf("enter the values of p,t,r\n");
+	scanf("%d%d%d",&p,&t,&r);
+	/*the formula of simplke intrest is (p*t*r/100)
+	p-->principle amount
+	r-->rate of intrest
+	t-->alwqays take monts some cases they prefers then change t*/
+	
+	si=(p*t*r/100);
+	
+	
+	printf("the simple intrest of that much of amount is %d",si);
+	
+	return 0;
+	}
