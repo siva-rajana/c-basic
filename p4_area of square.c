e102d81a3b5092fe894cdf13804245ebeba5f12d
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-02-24 12:10:09 +0530

    Add files via upload

---
+++ p4_area of square.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+{   
+
+    int s,a;
+    printf("enter the side of the square");
+    scanf("%d",&s);
+    
+    a=s*s;
+    
+    printf("the area of the square is %d",a);
+	
+	
+	
+	return 0;
+	
+}
