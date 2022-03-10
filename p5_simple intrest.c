f4328c6a228e957b648d611cab8392b5f604f4da
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-03-10 12:00:06 +0530

    Update p5_simple intrest.c

--- p5_simple intrest.c
+++ p5_simple intrest.c
@@ -4,7 +4,7 @@ int main()
 	int p,t,r,si;
 	printf("enter the values of p,t,r\n");
 	scanf("%d%d%d",&p,&t,&r);
-	/*the formula of simplke intrest is (p*t*r/100)
+	/*the formula of simple intrest is (p*t*r/100)
 	p-->principle amount
 	r-->rate of intrest
 	t-->alwqays take monts some cases they prefers then change t*/
