9e791af55826c901842850af68bb8c6d2d91b6cb
siva-rajana <92910266+siva-rajana@users.noreply.github.com>
2022-03-12 12:31:51 +0530

    Add files via upload

---
+++ if_elseif_months.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void main()
+{
+	int month;
+	printf("enter the month number\n");
+	scanf("%d",&month);
+	
+	if(month==1)
+	printf("jan\n");
+	
+		if(month==2)
+	printf("feb\n");
+		if(month==3)
+	printf("mar\n");
+	
+		if(month==4)
+	printf("apr\n");
+	
+		if(month==5)
+	printf("may\n");
+	
+		if(month==6)
+	printf("june\n");
+	
+		if(month==7)
+	printf("july\n");
+	
+		if(month==8)
+	printf("aug\n");
+	
+		if(month==9)
+	printf("sep\n");
+	
+		if(month==10)
+	printf("oct\n");
+	
+		if(month==11)
+	printf("nov\n");
+	
+		if(month==12)
+	printf("dec\n");
+	
+	else
+	{
+		
+printf("the given month number is invalid");
+		}	
+	
+	
+	
+	
+	
+	
+	
+}
